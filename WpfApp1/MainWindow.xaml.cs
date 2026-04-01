005b58b93050165cabfea19e24412c17ad8cac25
Milosz Puchalski <mpuchalski303@gmail.com>
2026-04-01 21:15:00 +0200

    Nauka WPF

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool Run = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (Run)
+            {
+                tbHello.Text = "Stop";
+                btnRun.Content = "Run";
+
+            }
+            else
+            {
+                tbHello.Text = "Running";
+                btnRun.Content = "Stop";
+            }
+            
+            Run = !Run;
+        }
+    }
+}
