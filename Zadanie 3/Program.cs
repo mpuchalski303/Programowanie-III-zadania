6120a2877e38921183755d125ffc430808af1014
Milosz Puchalski <mpuchalski303@gmail.com>
2026-05-10 15:21:19 +0200

    zad 3

---
+++ Zadanie 3/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Zadanie_3
+{
+    internal class Program
+    {
+        public interface IWypisz
+        {
+            string Wypisz();
+        }
+        public abstract class Figura
+        {
+            protected readonly DateTime Utworzono;
+            protected Figura()
+            {
+                Utworzono = DateTime.Now;
+            }
+        }
+        public abstract class Wielokat : Figura, IWypisz
+        {
+            public virtual string Wypisz()
+            {
+                return $"Wielokat: {Utworzono}";
+            }
+        }
+        public class kolo : Figura, IWypisz
+        {
+            public virtual string Wypisz()
+            {
+                return $"kolo: {Utworzono}";
+            }
+        }
+        public class Prostokat : Wielokat
+        {
+            public override string Wypisz()
+            {
+                return $"Prostokat: {Utworzono}";
+            }
+        }
+        public class Trojakt : Wielokat
+        {
+            public override string Wypisz()
+            {
+                return $"Trojakt: {Utworzono}";
+            }
+        }
+        public class Kwadrat : Prostokat
+        {
+            public override string Wypisz()
+            {
+                return $"Kwadrat: {Utworzono}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Wielokat wiel1 = new Kwadrat();
+            IWypisz kolo1 = new kolo();
+
+            Console.WriteLine(wiel1.Wypisz());
+            Console.WriteLine(kolo1.Wypisz());
+        }
+    }
+}
---
+++ cwiczenia/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace cwiczenia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MinMax(out int minint, out int maxint, 1, 2, 3, 4, 5, -10);
+                Console.WriteLine($"{minint}, {maxint}");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                MinMax(out float minint, out float maxint, 1.2f , 3.4f , 0.2f , 4, 12.3f, -10);
+                Console.WriteLine($"{minint}, {maxint}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                MinMax(out string minint, out string maxint, "jeden" , "dwa", "trzy");
+                Console.WriteLine($"{minint}, {maxint}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="args"></param>
+        public static void MinMax<T>(out T min, out T max, params T[] args) where T : IComparable<T>
+        {
+            if (args.Length == 0 || args == null)
+            {
+                throw new ArgumentException("Zle");
+            }
+            min = args[0];
+            max = args[0];
+            foreach(T item in args)
+            {
+                if(item.CompareTo(min) < 0)
+                {
+                    min = item;
+
+                }
+                if(item.CompareTo(max) > 0)
+                {
+                    max = item;
+                }
+            }
+        }
+    }
+}
